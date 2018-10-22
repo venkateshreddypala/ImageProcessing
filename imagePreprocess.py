2e70574fefd02156d1a56a498154f51ac8eefe17
Venkatesh <venkateshreddypala@gmail.com>
2018-10-22 18:55:48 -0400

    missing updates

--- imagePreprocess.py
+++ imagePreprocess.py
@@ -5,3 +5,37 @@ import PIL from Image
 
 # path for the raw image, we can also import OS and define a path
 # src_path = 
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite(src_path + "removed_noise.png", img)
+
+    #  Apply threshold to get image with only black and white
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite(src_path + "thres.png", img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+
+    # Remove template file
+    #os.remove(temp)
+
+    return result
+
+
+print ('Cleaning the image')
+print (get_string(src_path + "2.png"))
+
+print ('Cleaning process is complete')
